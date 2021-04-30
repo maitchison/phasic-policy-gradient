d7bf2fd60bdbdfa64a794f35faaad52df1830c26
Matthew Aitchison <matthew.aitchison@anu.edu.au>
2021-04-30 15:24:31 +1000

    added auto mode to runner

--- runner.py
+++ runner.py
@@ -30,23 +30,29 @@ class Job():
                 if k not in self.args:
                     self.args[k] = v
 
+    @property
+    def experiment_folder(self):
+        return f"./run/{self.experiment}"
+
+    @property
+    def job_folder(self):
+        return f"./run/{self.experiment}/{self.job_name}"
+
     def run(self, device):
         """
         Executes the job
         """
 
-        experiment_folder = f"./run/{self.experiment}"
-
         # 1. copy files to experiment folder for reference (if they are not already there)
         # assuming linux here...
-        if not os.path.exists(f"{experiment_folder}/train.py"):
+        if not os.path.exists(f"{self.experiment_folder}/train.py"):
             print("Copying source files...")
-            os.makedirs(experiment_folder, exist_ok=True)
-            os.system(f"cp train.py {experiment_folder}/")
-            os.system(f"cp -r phasic {experiment_folder}/")
+            os.makedirs(self.experiment_folder, exist_ok=True)
+            os.system(f"cp train.py {self.experiment_folder}/")
+            os.system(f"cp -r phasic {self.experiment_folder}/")
 
         # 2. execute the job
-        os.chdir(experiment_folder)
+        os.chdir(self.experiment_folder)
 
         workers = device.count(',') + 1
 
@@ -56,23 +62,39 @@ class Job():
         print(f"Running: {command_str}")
         os.system(command_str)
 
-JOBS = {
-    'e_aux_0': Job('e_aux', "e_aux=0", DEFAULT_PPO_ARGS, n_aux_epochs=0),
-    'e_aux_1': Job('e_aux', "e_aux=1", DEFAULT_PPO_ARGS, n_aux_epochs=1),
-    'e_aux_3': Job('e_aux', "e_aux=3", DEFAULT_PPO_ARGS, n_aux_epochs=3),
-    'e_aux_6': Job('e_aux', "e_aux=6", DEFAULT_PPO_ARGS, n_aux_epochs=6),
-    'e_aux_ppo': Job('e_aux', "ppo", DEFAULT_PPO_ARGS),
-}
+    def is_started(self):
+        return os.path.exists(self.job_folder)
 
-parser = argparse.ArgumentParser(description='Run a predefined job')
-parser.add_argument('job', type=str)
-parser.add_argument('--device', type=str, default='0')
+if __name__ == "__main__":
 
-args = parser.parse_args()
+    JOBS = {
+        'e_aux_0': Job('e_aux', "e_aux=0", DEFAULT_PPO_ARGS, n_aux_epochs=0),
+        'e_aux_1': Job('e_aux', "e_aux=1", DEFAULT_PPO_ARGS, n_aux_epochs=1),
+        'e_aux_3': Job('e_aux', "e_aux=3", DEFAULT_PPO_ARGS, n_aux_epochs=3),
+        'e_aux_6': Job('e_aux', "e_aux=6", DEFAULT_PPO_ARGS, n_aux_epochs=6),
+        'e_aux_ppo': Job('e_aux', "ppo", DEFAULT_PPO_ARGS),
+    }
 
-key = args.job.lower()
-if key not in JOBS:
-    raise ValueError(f"No job named {args.job}, try {list(JOBS.keys())}")
+    parser = argparse.ArgumentParser(description='Run a predefined job')
+    parser.add_argument('job_name', type=str)
+    parser.add_argument('--device', type=str, default='0')
 
-JOBS[key].run(device=args.device)
+    args = parser.parse_args()
+
+    if args.job_name == "auto":
+
+        while True:
+
+            todo_jobs = [v for k, v in JOBS.items() if not v.is_started()]
+            if len(todo_jobs) == 0:
+                print("All jobs finished.")
+                break
+
+            todo_jobs[0].run(device=args.device)
+    else:
+        key = args.job_name.lower()
+        if key not in JOBS:
+            raise ValueError(f"No job named {args.job_name}, try {list(JOBS.keys())}")
+
+        JOBS[key].run(device=args.device)
 
